8b9847bc2495c2be9db2babb9306c0d8fec1c429
klarkcharlz <klarkcharlz>
2024-01-03 15:17:15 +0300

    'add-first-routing'

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ import pydantic_models
 import database
 import config
 
-
 api = fastapi.FastAPI()
+
+response = {"Ответ": "Который возвращает сервер"}
+
+
+@api.get('/')
+def index():
+    return response
+
+
+@api.get('/hello')
+def hello():
+    return "hello"
+
+
+@api.get('/about/us')
+def about():
+    return {"We Are": "Legion"}
