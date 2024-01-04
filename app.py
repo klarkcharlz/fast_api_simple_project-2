8ca6368255236733c3410b43b3ac13b38c198239
klarkcharlz <klarkcharlz>
2024-01-04 16:14:26 +0300

    'add-url-and-query-params'

--- app.py
+++ app.py
@@ -9,19 +9,47 @@ import config
 
 api = fastapi.FastAPI()
 
-response = {"Ответ": "Который возвращает сервер"}
+fake_database = {'users': [
+    {
+        "id": 1,  # тут тип данных - число
+        "name": "Anna",  # тут строка
+        "nick": "Anny42",  # и тут
+        "balance": 15300  # а тут float
+    },
+
+    {
+        "id": 2,  # у второго пользователя
+        "name": "Dima",  # такие же
+        "nick": "dimon2319",  # типы
+        "balance": 160.23  # данных
+    }
+    , {
+        "id": 3,  # у третьего
+        "name": "Vladimir",  # юзера
+        "nick": "Vova777",  # мы специально сделаем
+        "balance": "25000"  # нестандартный тип данных в его балансе
+    }
+], }
 
 
-@api.get('/')
-def index():
-    return response
+@api.get('/get_info_by_user_id/{id:int}')
+def get_info_about_user(id):
+    return fake_database['users'][id - 1]
 
 
-@api.get('/hello')
-def hello():
-    return "hello"
+@api.get('/get_user_balance_by_id/{id:int}')
+def get_user_balance(id):
+    return fake_database['users'][id - 1]['balance']
 
 
-@api.get('/about/us')
-def about():
-    return {"We Are": "Legion"}
+@api.get('/get_total_balance')
+def get_total_balance():
+    total_balance: float = 0.0
+    for user in fake_database['users']:
+        total_balance += pydantic_models.User(**user).balance
+    return total_balance
+
+
+@api.get("/users/")
+def get_users(skip: int = 0, limit: int = 10):
+    return fake_database['users'][skip: skip + limit]
